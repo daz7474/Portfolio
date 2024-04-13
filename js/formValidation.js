9680258dd85ff14ed0a46aa76adcd7bb35c0e226
Darren Lindsay <daz7474@gmail.com>
2024-04-13 20:34:52 +0100

    Changed form validation

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,55 +1,70 @@
-// ======================================
-// Contact Form
-// ======================================
+// Form response
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  let formData = new FormData(this);
 
-// Get ref to input fields
-const form = document.getElementById("contact-form");
-const firstName = document.getElementById("contact-first-name");
-const lastName = document.getElementById("contact-last-name");
-const email = document.getElementById("contact-email");
-const msg = document.getElementById("contact-msg");
-const submitBtn = document.getElementById("contact-submit");
+  fetch(this.action, {
+    method: "POST",
+    body: formData
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw response;
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.errors) {
+        // Clear any messages
+        const successMsg = document.getElementById('success-message');
+        if (successMsg) successMsg.textContent = '';
 
-// Email validation
-let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        // Display error message
+        Object.keys(data.errors).forEach(key => {
+          let errorSpan = document.getElementById('error-' + key);
+          if (errorSpan) {
+            errorSpan.textContent = data.errors[key];
+          }
+        });
+      } else {
+        // Clear any messages
+        document.querySelectorAll('.error-span').forEach(span => span.textContent = '');
 
-// When submit button is clicked, check if fields have values
-submitBtn.addEventListener("click", (e) => {
+        // Display success message
+        let successSpan = document.getElementById('success-message');
+        if (successSpan) {
+          successSpan.textContent = data.success;
+          successSpan.style.padding = "10px";
+          // Clear the form
+          document.getElementById("contact-form").reset();
+          setTimeout(() => {
+            successSpan.textContent = '';
+            successSpan.style.padding = "0";
+          }, 4000);
+        }
+      }
+    })
+    .catch(error => {
+      error.json().then(body => {
+        // Clear any messages
+        const successMsg = document.getElementById('success-message');
+        if (successMsg) successMsg.textContent = '';
 
-  // Store empty fields in array
-  let required = [];
-
-  // Check if first name is empty
-  if (firstName.value === "" || firstName.value == null) {
-    firstName.placeholder = "First name required";
-    firstName.classList.add("required");
-    required.push("First name required");
-  }
-  // Check if last name is empty
-  if (lastName.value === "" || lastName.value == null) {
-    lastName.placeholder = "Last name required";
-    lastName.classList.add("required");
-    required.push("Last name required");
-  }
-  // Check email validation
-  if (!emailRegex.test(email.value)) {
-    email.placeholder = "Email required";
-    email.classList.add("required");
-    required.push("Email required");
-  }
-  // Check if message is empty
-  if (msg.value === "" || msg.value == null) {
-    msg.placeholder = "Message required"
-    msg.classList.add("required");
-    required.push("Message required");
-  }
-
-  // Prevent default if required fields are empty
-  if (required.length > 0) {
-    e.preventDefault;
-    return false;
-  }
-
-  // Submit form
-  form.submit();
+        // Handle HTTP response error
+        if (body && body.errors) {
+          Object.keys(body.errors).forEach(key => {
+            let errorSpan = document.getElementById('error-' + key);
+            if (errorSpan) {
+              errorSpan.textContent = body.errors[key];
+            }
+          });
+        } else {
+          // Generic error
+          let errorSpan = document.getElementById('error-general');
+          if (errorSpan) {
+            errorSpan.textContent = 'Failed to process form.';
+          }
+        }
+      });
+    });
 });
