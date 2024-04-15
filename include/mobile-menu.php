88ca7428b5080a44166f64b62099a49d1737f96f
Darren Lindsay <daz7474@gmail.com>
2024-04-15 13:00:25 +0100

    Updated links to point to correct files

---
+++ include/mobile-menu.php
@@ -0,0 +1,37 @@
+<div class="top-nav top grid-item">
+      <h1 class="mobile-initials">
+        <a class="text-light" href="index.html">
+          DL
+        </a>
+      </h1>
+      <button class="hamburger">
+        <span class="bar"></span>
+      </button>
+
+      <!-- Mobile Slide Menu -->
+
+      <div class="mobile-menu">
+
+        <div class="nav-content-mobile">
+          <a class="btn" href="about.php">About Me</a>
+          <a class="btn" href="#main">My Portfolio</a>
+          <a class="btn" href="coding.php">Coding Examples</a>
+          <a class="btn" href="scs.php">SCS Scheme</a>
+        </div>
+
+        <div class="contact-mobile">
+          <a class="btn" href="#form">Contact Me</a>
+        </div>
+
+        <div class="socials">
+          <a class="social-link" href="https://github.com/daz7474" target="_blank">
+            <i class="fa-brands fa-github"></i>
+          </a>
+          <a class="social-link" href="https://www.linkedin.com/in/darren-lindsay/" target="_blank">
+            <i class="fa-brands fa-linkedin-in"></i>
+          </a>
+        </div>
+
+      </div>
+
+    </div>
---
+++ include/side-nav.php
@@ -0,0 +1,33 @@
+<nav class="grid-item nav">
+      <div class="nav-container">
+
+        <div class="initials">
+          <h2>
+            <a href="index.php">
+              <span class="text-secondary">DL</span>
+            </a>
+          </h2>
+        </div>
+
+        <div class="nav-content">
+          <a class="btn" href="about.php">About Me</a>
+          <a class="btn" href="#main">My Portfolio</a>
+          <a class="btn" href="coding.php">Coding Examples</a>
+          <a class="btn" href="scs.php">SCS Scheme</a>
+        </div>
+
+        <div class="contact">
+          <a class="btn" href="#form">Contact Me</a>
+        </div>
+
+        <div class="socials">
+          <a class="social-link" href="https://github.com/daz7474" target="_blank">
+            <i class="fa-brands fa-github"></i>
+          </a>
+          <a class="social-link" href="https://www.linkedin.com/in/darren-lindsay/" target="_blank">
+            <i class="fa-brands fa-linkedin-in"></i>
+          </a>
+        </div>
+
+      </div>
+    </nav>
